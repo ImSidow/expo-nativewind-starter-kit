c06a84e5f9c0b87b9a0146ea14eff52150633004
Asad Ali Sidow <73076630+ImSidow@users.noreply.github.com>
2022-12-12 07:15:47 +0300

    Update Button.tsx

--- src/components/buttons/Button.tsx
+++ src/components/buttons/Button.tsx
@@ -2,7 +2,7 @@ import { Text, TextStyle, TouchableOpacity, TouchableOpacityProps, ViewStyle } f
 
 const Button: React.FC<{ label?: string; labelStyle?: TextStyle; labelClassName?: string; style?: ViewStyle; className?: string; children?: React.ReactNode } & TouchableOpacityProps> = ({ label, labelStyle, labelClassName, style, className, children, ...props }) => {
     return (
-        <TouchableOpacity style={style} className={`rounded px-10 py-2.5 self-start bg-[#3290ED] ${className}`} {...props}>
+        <TouchableOpacity style={style} className={`rounded px-10 py-2.5 self-start flex justify-center items-center bg-[#3290ED] ${className}`} {...props}>
             {children ? (
                 children
             ) : (
